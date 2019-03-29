5ec8e7d76f7319876254ab1471cb784ccaa89da1
Sławomir Paszko <folkus@gmail.com>
2019-03-29 15:46:39 +0100

    Update with newest pySerial changes.

--- socketCat.py
+++ socketCat.py
@@ -41,31 +41,43 @@ inputSize = os.stat(args.inputFile).st_size
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((args.ipaddress, args.port))
 
+# Variable with state of Rx Thread
+RxThreadRunning=0
+TxTransmitted=0
+
 # Reading thread
 def read():
+    global RxThreadRunning
+    global TxTransmitted
     print "Output to ",args.outputFile,"."
     outFile = open(args.outputFile,'w')
     readedBytes=0;
     maxNoDataTime=5 #[s]
     readStartTime=time.time()
     lastDataTime=time.time()
+    RxThreadRunning=1
     semaphoreStartSynchro.release()
     while ((readedBytes != inputSize) and ((time.time() - lastDataTime) < maxNoDataTime)):
         data = s.recv(256);
+        sys.stdout.write("\rTransmitted %d/%dB. Readed %d/%dB." % (TxTransmitted,inputSize,readedBytes,inputSize))
+        sys.stdout.flush()
         if len(data) > 0:
             outFile.write(data)
             readedBytes+=len(data)
             lastDataTime=time.time()
-            sys.stdout.write("\rReaded %d/%dB." % (readedBytes,inputSize))
-            sys.stdout.flush()
         else:
             print "\nNo data time",(time.time() - lastDataTime),"s."
+
     outFile.close()
+    sys.stdout.write("\rTransmitted %d/%dB. Readed %d/%dB." % (TxTransmitted,inputSize,readedBytes,inputSize))
     print "\nWhole read transfer time:",str(round((time.time()-readStartTime),2)),"s."
+    RxThreadRunning=0
     print "Transfer speed ",str(round((readedBytes/(time.time()-readStartTime))/1024,2)),"kB/s."
 
 # Writing thread
 def main():
+    global RxThreadRunning
+    global TxTransmitted
     # Read thread creation
     semaphoreStartSynchro.acquire()
     tRead = threading.Thread(target=read, args=())
@@ -80,6 +92,10 @@ def main():
     inFile = open(args.inputFile,'r')
     for line in inFile:
         s.send(line)
+        TxTransmitted   += len(line)
+        if (RxThreadRunning == 0):
+            break;
+
 
     # Wait on reading thread and close port
     tRead.join()
